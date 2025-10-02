5678f8f38ba766092bff778ca1801e72ea1697ab
Rahul Bisht <rahulbisht8886@gmail.com>
2025-10-02 16:28:17 +0530

    fixed issues

--- Medsta/src/Pages/NavBar.jsx
+++ Medsta/src/Pages/NavBar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="w-full fixed flex justify-between p-3 items-center top-0 left-0 z-50 bg-white/90 backdrop-blur-sm shadow-md">
+    <div className="w-full h-20 overflow-hidden fixed flex justify-between items-center top-0 left-0 z-50 bg-white/90 backdrop-blur-[2px] shadow-lg">
       <div className="logo flex items-center gap-0.5 tracking-widest text-xl font-semibold">
-        <img className="h-auto w-30" src="/Images/logo.png" alt="Logo" />
+        <img className="h-30 w-auto" src="/Images/logo.png" alt="Logo" />
       </div>
       <div className="Navigation py-2 px-6">
         <ul className="flex gap-6 text-[16px] text-gray-700">
@@ -22,7 +22,7 @@ const NavBar = () => {
           </li>
         </ul>
       </div>
-      <div className="Authenticate">
+      <div className="Authenticate px-8">
         <button className="bg-white text-blue-600 border border-blue-600 px-4 py-2 cursor-pointer rounded-md hover:bg-blue-50 transition-colors">
           Login
         </button>
